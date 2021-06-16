0ca25cc01dd96b89c8d3b015ea05307eef71857b
Boyce Estes <boyce.estes@Halohealth.com>
2021-06-16 09:29:48 -0400

    Create FeedItem model

---
+++ Sources/NetworkMe/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  
+//
+//  Created by Boyce Estes on 6/16/21.
+//
+
+import Foundation
+
+struct FeedItem {
+
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
