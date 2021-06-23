8013363e430335e7e4cf464cc13377de59214e2c
Boyce Estes <boyce.estes@Halohealth.com>
2021-06-23 08:37:28 -0400

    Test refactoring to remove duplication using factory method

--- Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
+++ Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
@@ -10,8 +10,9 @@ import XCTest
 
 class RemoteFeedLoader {
 
-    let url: URL?
+    let url: URL
     let client: HTTPClient
+
 
     init(url: URL, client: HTTPClient) {
 
@@ -19,10 +20,9 @@ class RemoteFeedLoader {
         self.client = client
     }
 
-    func load() {
 
-        // Modify the HTTPClient singleton:
-        client.get(from: url!)
+    func load() {
+        client.get(from: url)
     }
 }
 
@@ -33,56 +33,48 @@ protocol HTTPClient {
 }
 
 
-class HTTPClientSpy: HTTPClient {
-
-    var requestedURL: URL?
-
-    func get(from url: URL) {
-
-        requestedURL = url
-    }
-}
 
 
 class RemoteFeedLoaderTests: XCTestCase {
 
-
-    // when we are using RemoteFeedLoader, there are a couple key factors
-    // input: some URL that will retrieve some data using some HTTPClient
-    // output: some FeedItem
-
-    // Make sure that we do not have a requestedURL in the HTTPClient without calling load
-    // by the remoteFeedLoader.
     func test_init_doesNotRequestDataFromURL() {
 
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        let _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
-    // We need to connect the client to the feed loader somehow so that we can change
-    // the HTTPClient's requestedURL from the RemoteFeedLoader.
-    // Possible through dependency injection (initializer injection, property injection, or method injection)
-    // Or we could make HTTPClient a singleton so that it is available everywhere.
+
     func test_load_requestDataFromURL() {
 
         let url = URL(string: "https://a-specific-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
 
         sut.load()
 
         XCTAssertEqual(client.requestedURL, url)
     }
 
-    // There is no good reason that there cannot be multiple HTTPClients.
 
-    // The goal is to get rid of the singleton so that we can mock the HTTPClient.
-    // One way to do this:
+    // MARK: Helpers
 
-    // Change this singleton we can make `shared` mutable. This makes this a global state
-    // since it can be changed. This is useful because we don'
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
+
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+
+    class HTTPClientSpy: HTTPClient {
+
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+
+            requestedURL = url
+        }
+    }
+
 }
 
