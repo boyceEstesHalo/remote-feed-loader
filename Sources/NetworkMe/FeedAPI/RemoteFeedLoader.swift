1842c7b07444a6083caf90530db265de3e261663
Boyce Estes <boyce.estes@Halohealth.com>
2021-06-24 01:04:16 -0400

    Move RemoteFeedLoader to production

---
+++ Sources/NetworkMe/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,33 @@
+//
+//  RemoteFeedLoader.swift
+//  
+//
+//  Created by Boyce Estes on 6/24/21.
+//
+
+import Foundation
+
+
+public protocol HTTPClient {
+
+    func get(from url: URL)
+}
+
+
+public final class RemoteFeedLoader {
+
+    private let url: URL
+    private let client: HTTPClient
+
+
+    public init(url: URL, client: HTTPClient) {
+
+        self.url = url
+        self.client = client
+    }
+
+
+    public func load() {
+        client.get(from: url)
+    }
+}
--- Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
+++ Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
@@ -6,31 +6,9 @@
 //
 
 import XCTest
+import NetworkMe
 
 
-class RemoteFeedLoader {
-
-    let url: URL
-    let client: HTTPClient
-
-
-    init(url: URL, client: HTTPClient) {
-
-        self.url = url
-        self.client = client
-    }
-
-
-    func load() {
-        client.get(from: url)
-    }
-}
-
-
-protocol HTTPClient {
-
-    func get(from url: URL)
-}
 
 
 
