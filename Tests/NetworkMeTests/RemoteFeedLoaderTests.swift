b8ee1aebe5fcba83b78b7977bdf0fc1ab9b84960
Boyce Estes <boyce.estes@Halohealth.com>
2021-06-26 11:31:13 -0400

    Created custom result type to simplify error/response handling.

--- Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
+++ Tests/NetworkMeTests/RemoteFeedLoaderTests.swift
@@ -95,7 +95,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     class HTTPClientSpy: HTTPClient {
 
-        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
         var requestedURLs: [URL] {
 
@@ -103,7 +103,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
 
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 
             messages.append((url, completion))
         }
@@ -111,8 +111,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         func complete(with error: Error, index: Int = 0) {
 
-
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
 
 
@@ -123,8 +122,9 @@ class RemoteFeedLoaderTests: XCTestCase {
                 statusCode: statusCode,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            
+            messages[index].completion(.success(response))
         }
     }
 }
