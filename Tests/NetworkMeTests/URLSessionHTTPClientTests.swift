30a7dd5cbc71b4d4154472d8a80e1479cbe30614
Boyce Estes <boyce.estes@Halohealth.com>
2021-08-18 17:41:58 -0400

    Move the URLSessionHTTPClient(the system under test ("SUT")) to a factory method

--- Tests/NetworkMeTests/URLSessionHTTPClientTests.swift
+++ Tests/NetworkMeTests/URLSessionHTTPClientTests.swift
@@ -59,7 +59,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
 
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
 
@@ -73,11 +73,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
 
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        let sut = URLSessionHTTPClient()
 
         let exp = expectation(description: "Wait for completion")
 
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case .failure(let receivedError as NSError):
                 // in iOS 14 receivedError is returned as different NSError instances.
@@ -95,6 +94,12 @@ class URLSessionHTTPClientTests: XCTestCase {
 
 
     // MARK: - Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+
+        return URLSessionHTTPClient()
+    }
+
+
     private class URLProtocolStub: URLProtocol {
 
         private struct Stub {
