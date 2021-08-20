29fcfc3a0d6965846d62bea84e58e6f34e9db378
Boyce Estes <boyce.estes@Halohealth.com>
2021-08-20 12:01:54 -0400

    Delivers error on all invalid or unexpected responses

--- Tests/NetworkMeTests/URLSessionHTTPClientTests.swift
+++ Tests/NetworkMeTests/URLSessionHTTPClientTests.swift
@@ -82,9 +82,25 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 
 
-    func test_URLSessionHTTPClient_getFromURL_failsOnAllNilValues() {
+    func test_URLSessionHTTPClient_getFromURL_failsOnAllInvalidRepresentationCases() {
 
+        let anyData = Data(bytes: "any data", count: "any data".count)
+        let anyError = NSError(domain: "any error", code: 0)
+        let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+
+        // If we were not it is possible they could return some other error, should possibly compare if it is
+        // UnexpectedValuesRepresentation
         XCTAssertNotNil(resultErrorFor(data: nil, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: nil))
     }
 
 
